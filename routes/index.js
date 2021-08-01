f0029295ed9d909ab1128155cd2130dac2b21375
yamkim <coramdeo0708@gmail.com>
2021-08-01 20:48:35 +0900

    refactor: indexRouteHandler 객체에 함수 추가
    - NodeBirdApi와 형식을 유사하게 맞춥니다.
    - returnAllPostsForUser: 요청한 유저가 작성한 모든 게시물에 대한 api 요청을 보냅니다.
    - returnAllPostsAboutHashtag: 요청한 해쉬태그가 달린 모든 게시물에 대한 api 요청을 보냅니다.

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const apiURL = 'http://localhost:8002/v1';
 axios.defaults.headers.origin = 'http://localhost:4000';
 
-const request = async (req, api) => {
+const axiosNodeBirdApi = async (req, api) => {
   try {
     if (!req.session.jwt) {
       const tokenResult = await axios.post(`${apiURL}/token`, {
@@ -14,7 +14,6 @@ const request = async (req, api) => {
       });
       req.session.jwt = tokenResult.data.token;
     }
-
     return await axios.get(`${apiURL}${api}`, {
       headers: { authorization: req.session.jwt },
     });
@@ -27,54 +26,29 @@ const request = async (req, api) => {
   }
 }
 
-router.get('/mypost', async (req, res, next) => {
+const returnJsonData = async function (req, res, next) {
   try {
-    const result = await request(req, '/posts/my');
+    const result = await axiosNodeBirdApi(req, req.apiUrl);
     res.json(result.data);
   } catch (err) {
     console.error(err);
     next(err);
   }
-});
+}
 
-router.get('/search/:hashtag', async (req, res, next) => {
-  try {
-    const result = await request(
-      req, `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`,
-    );
-    res.json(result.data);
-  } catch (err) {
-    if (err.code) {
-      console.error(err);
-      next(err);
-    }
+const IndexRouteHandler = {
+  returnAllPostsForUser: async function(req, res, next) {
+    req.apiUrl = '/posts/my';
+    returnJsonData(req, res, next);
+  },
+
+  returnAllPostsAboutHashtag: async function(req, res, next) {
+    req.apiUrl = `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`;
+    returnJsonData(req, res, next);
   }
-});
+}
 
-// router.get('/test', async (req, res, next) => {
-//   try {
-//     if (!req.session.jwt) { // 세션에 토큰이 없으면 발급을 시도합니다.
-//       const tokenResult = await axios.post('http://localhost:8002/v1/token', {
-//         clientSecret: process.env.CLIENT_SECRET,
-//       });
-//       if (tokenResult.data && tokenResult.data.code === 200) { // 토큰 발급이 성공하는 경우입니다.
-//         req.session.jwt = tokenResult.data.token;
-//       } else {                                                 // 토큰 발급 실패시, data에 실패 사유가 기입됩니다.
-//         return res.json(tokenResult.data);
-//       }
-//     }
-//     // 발급받은 토큰 테스트
-//     const result = await axios.get('http://localhost:8002/v1/test', {
-//       headers: { authorization: req.session.jwt },
-//     });
-//     return res.json(result.data);
-//   } catch (error) {
-//     console.error(error);
-//     if (error.response.status === 419) {
-//       return res.json(error.response.data);
-//     }
-//     return next(error);
-//   }
-// });
+router.get('/mypost', IndexRouteHandler.returnAllPostsForUser);
+router.get('/search/:hashtag', IndexRouteHandler.returnAllPostsAboutHashtag);
 
 module.exports = router;
